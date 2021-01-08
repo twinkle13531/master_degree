69880474103aac0b84437da1d41016a9e4491a3c
twinkle13531 <shihosato1234@gmail.com>
2021-01-08 17:03:47 +0900

    new results

--- 202012/SA/Neal_exact_test_functions.py
+++ 202012/SA/Neal_exact_test_functions.py
@@ -78,32 +78,29 @@ def find_valid_y(res):
                 valid_y_info_dic[this_time_y] = y_info[2]
     return valid_y_info_dic
 
-def calc_p_value(df, res):                                                        
-    valid_y_list= []                                                           
-    valid_y_num= 0
+
+def calc_p_value(valid_y_info_dic):
+    valid_y_list = list(valid_y_info_dic.kesy())
     t1_y = 0
     t1 = np.dot(df['Y'], df['LI'])
-    for y_info in list(res.record):
-        if y_info[1]==0.:
-            valid_y = list(y_info[0]) 
-            if all(valid_y != p for p in valid_y_list):
-                valid_y_num += 1
-                valid_y_list.append(valid_y)
-                if int(np.dot(valid_y, list(df['LI'])))==t1:
-                    t1_y += 1
-    return t1_y/valid_y_num
+    for valid_y_tu in valid_y_list:
+        valid_y = list(valid_y_tu)
+        if int(np.dot(valid_y, list(df['LI'])))==t1:
+            t1_y += 1
+    return t1_y/len(valid_y_info_dic)
 
-def y_num_hist(df, valid_y_list, path):
+
+def y_num_hist(df, valid_y_info_dic, path):
     LI = list(df['LI'])
     hist_dic = {}
+    valid_y_list = list(valid_y_info_dic.keys())
     for valid_y in valid_y_list:
-        t1 = int(np.dot(LI, valid_y))
+        valid_y_list = list(valid_y)
+        t1 = int(np.dot(LI, valid_y_list))
         if t1 in hist_dic.keys():
             hist_dic[t1] += 1
         else:
             hist_dic[t1] = 1
-            
-    
     x = [i for i in list(hist_dic.keys())]
     y = [i for i in list(hist_dic.values())]
     plt.xlabel('value of t1')
@@ -111,114 +108,78 @@ def y_num_hist(df, valid_y_list, path):
     plt.bar(x, y)
     plt.xticks(x, x)
     plt.yticks(y, y)
-    plt.savefig(path)
+    plt.savefig(path, format='png')
+    plt.close()
     return hist_dic
 
-def occurence_hist(occurrence_list, plot_path):
-    x = [i for i in range(len(occurrence_list))]
+
+def occurence_hist(valid_y_info_dic, plot_path):
+    x = [i for i in list(valid_y_info_dic.values())]
     plt.xlabel('each sample')
     plt.ylabel('number of the occurrence')
     plt.bar(x, occurrence_list)
     ax = plt.gca()
     ax.axes.xaxis.set_visible(False)
-    plt.savefig(plot_path)
-    return plt.show()
+    plt.savefig(plot_path, format='png')
+    plt.show()
+    plt.close()
+    return 0
+ 
     
 def time_num_y(df, num_reads, path):
-    time_list = []
-    time_0 = time.time() 
     sa_sampler = neal.sampler.SimulatedAnnealingSampler()
-    
     initial_states = df['Y'].values.tolist()
     t_list = calc_marginals(df)
     
     valid_y_list= []                                                           
     valid_y_num= 0
     bqm = make_Hamiltonian(df)
+    
+    time_0 = time.time()
     res = sa_sampler.sample(
         bqm, num_reads=num_reads, 
         initial_states=initial_states, 
         initial_states_generator='tile'
     )                                                      
+    time_list = []
+    valid_y_info_dic_1 = {}
     for y_info in list(res.record):
         if y_info[1]==0.:
-            if len(valid_y_list)==0:
-                valid_y_list.append(list(y_info[0]))
-                valid_y_num += 1
+            this_time_y = tuple(y_info[0])#sample
+            if this_time_y in list(valid_y_info_dic_1.keys()):
+                continue
+            else:
+                valid_y_info_dic_1[this_time_y] = 1
                 time_1 = time.time()
                 elapsed_time = time_1 - time_0
                 time_list.append(elapsed_time)
-                
-            elif all(list(y_info[0]) != p for p in valid_y_list): 
-                valid_y_list.append(list(y_info[0]))
-                valid_y_num += 1
-                time_1 = time.time()
-                elapsed_time = time_1 - time_0
-                time_list.append(elapsed_time)
-    
-    valid_y_num_list = [i for i in range(1, valid_y_num+1)]
-    
+    valid_y_num_list = [i for i in range(1, len(valid_y_info_dic_1)+1)]
     plt.xlabel('time')
     plt.ylabel('number of hits')
     plt.plot(time_list, valid_y_num_list)
     plt.savefig(path)
     plt.show()
-    return valid_y_list, valid_y_num_list, time_list
+    plt.close()
+    return time_list
 
 
-def p_value_transition(df, num_reads, output_path) :
-    sa_sampler = neal.sampler.SimulatedAnnealingSampler()
-    
-    initial_states = df['Y'].values.tolist()
-    t_list = calc_marginals(df)
-    t1 = int(np.dot(df['Y'], df['LI']))
-    t1_y = 0
-    p_dic = {}
-                                                               
-    valid_y_num= 0
+def p_value_transition(res, output_path):
     valid_y_list = []
-    bqm = make_Hamiltonian(df)
-    res = sa_sampler.sample(
-        bqm, num_reads=num_reads, 
-        initial_states=initial_states, 
-        initial_states_generator='tile'
-    )
-    
-    
+    t1_y = 0
+    t1 = int(np.dot(df['Y'], df['LI']))
+    p_dic = {}
     for y_info in list(res.record):
         if y_info[1]==0.:
             valid_y = list(y_info[0]) 
             if all(valid_y != p for p in valid_y_list):
-                valid_y_num += 1
                 valid_y_list.append(valid_y)
                 if int(np.dot(valid_y, list(df['LI'])))==t1:
                     t1_y += 1
-                    p_dic[valid_y_num] = t1_y/valid_y_num
-                    
+                    p_dic[len(valid_y_list)] = t1_y/len(valid_y_list)             
     plt.xlabel('number of hits')
     plt.ylabel('p value')
     plt.plot(list(p_dic.keys()), list(p_dic.values()))
     plt.savefig(output_path)
     plt.show()
-    
-    return valid_y_num, valid_y_list, p_dic
-
-
-def test_find_valid_y():
-    df = pd.read_csv('../../input/ost20.csv', sep=',', index_col=0)
-    valid_y_list, valid_y_num = find_valid_y(df,  num_reads = 10)
-    return valid_y_list, valid_y_num
-
-
-
-def test_validity(canditate_list):
-    df1 = pd.read_csv('../../input/ost20.csv', sep=',',index_col=0)
-    df2 = pd.read_csv('../../input/ost20.csv', sep=',',index_col=0)
-    new_y = np.array(canditate_list)
-    df2['Y'] = new_y
-    t_list1 = calc_marginals(df1)
-    t_list2 = calc_marginals(df2)
-    print(t_list1)
-    print(t_list2)
-    assert np.all(t_list1[[0,2]] == t_list2[[0,2]])
-
+    plt.close()
+    return valid_y_list, p_dic
