25c604d86ee535aec73c7bf3319457d6e1786b0f
twinkle13531 <shihosato1234@gmail.com>
2021-01-10 13:32:51 +0900

    lost data, so do calculatoin again

--- 202012/4ti2/functions/zsolve_t1.py
+++ 202012/4ti2/functions/zsolve_t1.py
@@ -13,75 +13,50 @@ def make_result_list(zinhom_path):
         for i in range(1, zinhom_num+1):
             int_y = [int(l_strip[i][0].split()[j]) for j in range(len(l_strip[i][0].split()))]
             zinhom_list.append(int_y)
-    #print(zinhom_num)
     return zinhom_num, zinhom_list
 
-
-def calc_marginals(df):                   
-    return np.array([                      
-        sum(df['Y']),   
-        np.dot(df['Y'], df['SEX']),      
-        np.dot(df['Y'], df['AOP']),      
-    ])                                 
-
-
-def y_num_hist(df, zinhom_list, output_path):
-    LI = list(df['LI'])
-    hist_dic = {}
-    for zinhom in zinhom_list:
-        t1 = int(np.dot(LI, zinhom))
-        if t1 in hist_dic.keys():
-            hist_dic[t1] += 1
-        else:
-            hist_dic[t1] = 1
-
-    real_t1 = np.dot(df['Y'], df['LI'])
-    x = [i for i in list(hist_dic.keys())]
-    y = [i for i in list(hist_dic.values())]
-    plt.xlabel('value of t1')
-    plt.ylabel('number of samples')
-    plt.bar(x, y)
-    plt.xticks(x, x)
-    plt.yticks(y, y)
-    plt.plot(real_t1, 0, marker='o', markersize=20)
-    plt.savefig(output_path)
-    plt.show()
-    plt.close()
-    
-    return hist_dic
+class zsolve_result_summerize():
+    def __init__(self, df, zinhom_list):
+        self.df = df
+        self.zinhom_list = zinhom_list
+        
+    def calc_marginals(self):                   
+        return np.array([                      
+            sum(self.df['Y']),
+            np.dot(self.df['Y'], self.df['SEX']),      
+            np.dot(self.df['Y'], self.df['AOP']),      
+        ])                                 
 
 
-def y_num_hist_all_t1(df, zinhom_list, output_path):
-    LI = list(df['LI'])
-    hist_dic = {}
-    for zinhom in zinhom_list:
-        t1 = int(np.dot(LI, zinhom))
-        if t1 in hist_dic.keys():
-            hist_dic[t1] += 1
-        else:
-            hist_dic[t1] = 1
-            
-    for x in range(max(hist_dic.keys())+1):
-        if not x in hist_dic.keys():
-            hist_dic[x] = 0
-            
-    x = [i for i in list(hist_dic.keys())]
-    y = [i for i in list(hist_dic.values())]
-    plt.xlabel('value of t1')
-    plt.ylabel('number of samples')
-    plt.bar(x, y)
-    plt.xticks(x, x)
-    plt.yticks(y, y)
-    plt.savefig(output_path)
-    plt.show()
-    
-    return hist_dic
+    def y_num_hist(self, output_path):
+        LI = list(self.df['LI'])
+        hist_dic = {}
+        for zinhom in self.zinhom_list:
+            t1 = int(np.dot(LI, zinhom))
+            if t1 in hist_dic.keys():
+                hist_dic[t1] += 1
+            else:
+                hist_dic[t1] = 1
+
+        real_t1 = np.dot(self.df['Y'], self.df['LI'])
+        x = [i for i in list(hist_dic.keys())]
+        y = [i for i in list(hist_dic.values())]
+        plt.xlabel('value of t1')
+        plt.ylabel('number of samples')
+        plt.bar(x, y)
+        plt.xticks(x, x)
+        plt.yticks(y, y)
+        plt.plot(real_t1, 0, marker='o', markersize=20)
+        plt.savefig(output_path)
+        plt.show()
+        plt.close()
+        return hist_dic
 
 
-def calucurate_p(df, zinhom_list):
-    LI = list(df['LI'])
-    t1_y_num = 0
-    for zinhom in zinhom_list:
-        if int(np.dot(LI, df['Y'])) == int(np.dot(LI, zinhom)):
-            t1_y_num += 1
-    return t1_y_num / len(zinhom_list), len(zinhom_list)
+    def calucurate_p(self):
+        LI = list(self.df['LI'])
+        t1_y_num = 0
+        for zinhom in self.zinhom_list:
+            if int(np.dot(LI, self.df['Y'])) == int(np.dot(LI, zinhom)):
+                t1_y_num += 1
+        return t1_y_num / len(self.zinhom_list)
