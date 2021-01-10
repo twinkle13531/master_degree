424e4833ce40f698d51d4b6795ecd00a6be9a71f
twinkle13531 <shihosato1234@gmail.com>
2021-01-10 13:36:14 +0900

    modified the code

--- 202012/Random/random_exact_test_functions.py
+++ 202012/Random/random_exact_test_functions.py
@@ -15,14 +15,12 @@ def calc_marginals(df):
         np.dot(df['Y'], df['AOP']),      
     ])                               
 
-t_list = calc_marginals(df)
+#t_list = calc_marginals(df)
 
 #calculate_timeはこの関数を計測
 def find_valid_y(df, num_reads):    
     t_list = calc_marginals(df)
-    valid_y_num= 0
-    valid_y_dic = {}
-    time_0 = time.time()
+    valid_y_dic = {}#sample:occurrence_count
     y_lists = np.random.randint(2, size=(num_reads, df.shape[0])) #random                                
     for canditate_y in y_lists:                                                     
         if sum(canditate_y) == t_list[0]:#int同士の比較                      
@@ -35,9 +33,7 @@ def find_valid_y(df, num_reads):
                     valid_y_dic[canditate_y_tuple] += 1
                 else:
                     valid_y_dic[canditate_y_tuple] = 1                      
-    time_1 = time.time()
-    calculation_time = time_1 - time_0
-    return valid_y_dic, calculation_time
+    return valid_y_dic
 
 
 class Random_method:
@@ -71,26 +67,36 @@ class Random_method:
         plt.savefig(plot_path)
         return t1_dic
     
+    def occurrence_hist(self, plot_path):
+        occurrence_list = list(self.valid_y_dic.values())
+        x = [i for i in range(len(occurrence_list))]
+        plt.xlabel('each sample')
+        plt.ylabel('number of the occurrence')
+        plt.bar(x, occurrence_list)
+        ax = plt.gca()
+        ax.axes.xaxis.set_visible(False)
+        plt.savefig(plot_path)
+        plt.show()
+        plt.close()
+        return 0
+    
 
     def p_value(self):
         t1 = int(np.dot(self.df['Y'], self.df['LI']))
         LI = list(self.df['LI'])
         p_num = 0
-        for valid_y in self.valid_y_list:
+        for valid_y_tu in list(self.valid_y_dic.keys()):
+            valid_y = list(valid_y_tu)
             if t1 == int(np.dot(valid_y, LI)):
                 p_num += 1
-        if self.valid_y_num > 0:
-            return p_num/self.valid_y_num
-        else:
-            messa = "can't calculate p value"
-            return messa
+        if len(self.valid_y_dic) > 0:
+            return p_num/len(self.valid_y_dic)
     
     def p_value_transition(self, output_path):
         t1 = int(np.dot(self.df['Y'], self.df['LI']))
         LI = list(self.df['LI'])
         t1_y = 0
         p_dic = {}
-        valid_y_num_ever = 0
         valid_y_list_ever = []
         y_lists = np.random.randint(2, size=(self.num_reads, self.df.shape[0])) #random                                
         for canditate_y in y_lists:                                                     
@@ -101,11 +107,10 @@ class Random_method:
                 else:
                     canditate_y_list = list(canditate_y)
                     if all([canditate_y_list != p for p in valid_y_list_ever]):
-                        valid_y_num_ever += 1
                         valid_y_list_ever.append(canditate_y_list)
                         if int(np.dot(canditate_y_list, LI))==t1:
                             t1_y += 1
-                            p_dic[t1_y] = t1_y/valid_y_num_ever
+                            p_dic[t1_y] = t1_y/len(valid_y_list_ever)
         plt.xlabel('number of hits')
         plt.ylabel('p value')
         plt.plot(list(p_dic.keys()), list(p_dic.values()))
@@ -115,12 +120,10 @@ class Random_method:
         return p_dic
 
     
-    def time_num_y_transition(self, path):
+    def num_y_transition(self, path):
         time_0 = time.time()
         valid_ys = []
-        
-        time_list = [0]
-        valid_y_number = [0]                                 
+        time_list = []                               
         y_lists = np.random.randint(2, size=(self.num_reads, self.df.shape[0]))
         for canditate_y in y_lists:                                                     
             if sum(canditate_y) == self.t_list[0]:#int同士の比較                      
@@ -131,16 +134,16 @@ class Random_method:
                     canditate_y_list = list(canditate_y)
                     if all([canditate_y_list != p for p in valid_ys]):
                         valid_ys.append(canditate_y_list)
-                        valid_y_number.append(valid_y_number[-1]+1)
                         
                         time_1 = time.time()
                         elapsed_time = time_1 - time_0
                         time_list.append(elapsed_time)
 
+        valid_y_num_list = [i for i in range(1, len(time_list)+1)]
         plt.xlabel('time')
         plt.ylabel('number of hits')
-        plt.plot(time_list, valid_y_number)
+        plt.plot(time_list, valid_y_num_list)
         plt.savefig(path)
         plt.show()
         plt.close()
-        return valid_ys, time_list, valid_y_number    
+        return valid_ys, time_list   
